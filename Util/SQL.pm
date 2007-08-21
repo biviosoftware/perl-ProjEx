4037dc087839af5116296a5665cd646e6b302a24
Rob Nagler <github@q33.us>
2007-08-21 13:14:25 +0000

    SiteForum & TestUser called fixed up comments to show init_dbms and not depend on our .bashrc

--- Util/SQL.pm
+++ Util/SQL.pm
@@ -4,8 +4,9 @@ package ProjEx::Util::SQL;
 use strict;
 use Bivio::Base 'Bivio::Util::SQL';
 
-# su - postgres -c 'createuser --no-createdb --no-adduser --pwprompt projexuser; createdb --owner projexuser projex'
-# b_projex; cd files/ddl; ln -s ../../../Bivio/PetShop/files/ddl/bOP-tables.sql bOP-tables.sql; ln -s ../../../Bivio/PetShop/files/ddl/bOP-constraints.sql bOP-constraints.sql; ln -s ../../../Bivio/PetShop/files/ddl/bOP-sequences.sql bOP-sequences.sql
+# export BCONF=~/bconf/projex.bconf
+# cd files/ddl
+# perl -w ../../Util/projex-sql init_dbms
 # perl -w ../../Util/projex-sql create_test_db
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
@@ -14,18 +15,12 @@ sub ddl_files {
     return shift->SUPER::ddl_files(['bOP', 'projex']);
 }
 
+sub initialize_db {
+    return shift->call_super_before(\@_, sub {shift->new_other('SiteForum')->init});
+}
+
 sub initialize_test_data {
-    my($self) = @_;
-    my($req) = $self->get_request;
-    $self->create_test_user('buser');
-    $self->create_test_user('root');
-    $self->new_other('RealmRole')->make_super_user;
-    $req->set_realm(undef);
-    $self->model(ForumForm => {
-        'RealmOwner.display_name' => 'Unit Test Forum',
-	'RealmOwner.name' => 'bforum',
-    });
-    return;
+    return shift->call_super_before(\@_, sub {shift->new_other('TestUser')->init});
 }
 
 1;
