95378ae01e1816e049c1976cab7b2e256d55829b
Rob Nagler <github@q33.us>
2012-02-18 17:15:35 +0000

    use CURRENT_VERSION fmt

--- BConf.pm
+++ BConf.pm
@@ -8,49 +8,50 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub dev_overrides {
     my($proto, $pwd, $host, $user, $http_port) = @_;
-    return {
-    };
+    return {};
 }
 
 sub merge_overrides {
     my($proto, $host) = @_;
-    return Bivio::IO::Config->merge_list({
-	$proto->merge_class_loader({
-	    delegates => [
-		'Bivio::Agent::TaskId',
-		'Bivio::TypeError',
-	    ],
-	    maps => {
-		Action => ['ProjEx::Action'],
-		Delegate => ['ProjEx::Delegate'],
-		Facade => ['ProjEx::Facade'],
-		Model => ['ProjEx::Model'],
-		ShellUtil => ['ProjEx::Util'],
-		TestLanguage => ['ProjEx::Test'],
-		Type => ['ProjEx::Type'],
-		View => ['ProjEx::View'],
+    return Bivio::IO::Config->merge_list(
+	{
+	    $proto->merge_class_loader({
+		delegates => [
+		    'Bivio::Agent::TaskId',
+		    'Bivio::TypeError',
+		],
+		maps => {
+		    Action => ['ProjEx::Action'],
+		    Delegate => ['ProjEx::Delegate'],
+		    Facade => ['ProjEx::Facade'],
+		    Model => ['ProjEx::Model'],
+		    ShellUtil => ['ProjEx::Util'],
+		    TestLanguage => ['ProjEx::Test'],
+		    Type => ['ProjEx::Type'],
+		    View => ['ProjEx::View'],
+		},
+	    }),
+	    'Bivio::UI::Facade' => {
+		default => 'ProjEx',
+		http_suffix => 'www.PROD-DOMAIN',
+		mail_host => 'PROD-DOMAIN',
 	    },
-	}),
-	'Bivio::UI::Facade' => {
-	    default => 'ProjEx',
-	    http_suffix => 'www.PROD-DOMAIN',
-	    mail_host => 'PROD-DOMAIN',
+	    $proto->merge_http_log(
+		ignore_list => [
+		],
+		error_list => [
+		],
+		critical_list => [
+		],
+	    }),
 	},
-	$proto->merge_http_log({
-	    ignore_list => [
-	    ],
-	    error_list => [
-	    ],
-	    critical_list => [
-	    ],
+	$proto->default_merge_overrides({
+	    version => $proto->CURRENT_VERSION,
+	    root => 'ProjEx',
+	    prefix => 'projex',
+	    owner => 'COPYRIGHT-HOLDER',
 	}),
-    },
-    $proto->default_merge_overrides({
-	version => 10,
-	root => 'ProjEx',
-	prefix => 'projex',
-	owner => 'COPYRIGHT-HOLDER',
-    }));
+    );
 }
 
 1;
