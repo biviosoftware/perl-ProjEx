e1123c5ced010f00034eeb1c88abb34e58c3c4c1
Rob Nagler <github@q33.us>
2012-10-18 19:59:32 +0000

    *** empty log message ***

---
+++ UI/ViewShortcuts.pm
@@ -0,0 +1,9 @@
+# Copyright (c) COPYRIGHT-YEAR COPYRIGHT-HOLDER  All rights reserved.
+# $Id$
+package ProjEx::ViewShortcuts;
+use Bivio::Base 'UI';
+use Bivio::UI::ViewLanguageAUTOLOAD;
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+1;
---
+++ UIXHTML/ViewShortcuts.pm
@@ -0,0 +1,9 @@
+# Copyright (c) COPYRIGHT-YEAR COPYRIGHT-HOLDER  All rights reserved.
+# $Id$
+package ProjEx::UIXHTML::ViewShortcuts;
+use strict;
+use Bivio::Base 'UIXHTML';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+1;
