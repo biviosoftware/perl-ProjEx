a6d0f59a610bb06dd232647321be952332697a8e
Rob Nagler <github@q33.us>
2012-10-18 20:04:07 +0000

    moved to UI folder

--- ViewShortcuts.pm
+++
@@ -1,9 +0,0 @@
-# Copyright (c) COPYRIGHT-YEAR COPYRIGHT-HOLDER  All rights reserved.
-# $Id$
-package ProjEx::ViewShortcuts;
-use Bivio::Base 'Bivio::UI::XHTML::ViewShortcuts';
-use Bivio::UI::ViewLanguageAUTOLOAD;
-
-our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-
-1;
