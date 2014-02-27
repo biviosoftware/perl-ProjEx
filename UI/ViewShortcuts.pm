93733b52fc54f8a48aecc0a023acc6147661c683
Paul Moeller <moeller@bivio.biz>
2014-02-27 19:14:10 +0000

    removed

--- UI/ViewShortcuts.pm
+++
@@ -1,9 +0,0 @@
-# Copyright (c) COPYRIGHT-YEAR COPYRIGHT-HOLDER  All rights reserved.
-# $Id$
-package ProjEx::UI::ViewShortcuts;
-use Bivio::Base 'UI';
-use Bivio::UI::ViewLanguageAUTOLOAD;
-
-our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-
-1;
